3252fc6b980c78e1e5976fe8c4e0add36107341e
Kleber Correia <klebervirgilio@gmail.com>
2015-11-18 09:56:39 +1000

    Add origin function to vendor

--- vendor/assets/javascripts/tinymce/plugins/uploadimage/editor_plugin.js
+++ vendor/assets/javascripts/tinymce/plugins/uploadimage/editor_plugin.js
@@ -1,15 +1,20 @@
 (function() {
   tinymce.PluginManager.requireLangPack('uploadimage');
+  var currentOrigin = function(url){
+    var parts = url.split(/\/\/?/);
+    parts[1] = location.origin;
+    return parts.slice(1).join("/");
+  }
   tinymce.create('tinymce.plugins.UploadImagePlugin', {
     init: function(ed, url) {
       ed.addCommand('mceUploadImage', function() {
         return ed.windowManager.open({
-          file: url + '/dialog.html',
+          file: currentOrigin(url) + '/dialog.html',
           width: 350 + parseInt(ed.getLang('uploadimage.delta_width', 0)),
           height: 220 + parseInt(ed.getLang('uploadimage.delta_height', 0)),
           inline: 1
         }, {
-          plugin_url: url
+          plugin_url: currentOrigin(url)
         });
       });
       ed.addButton('uploadimage', {
